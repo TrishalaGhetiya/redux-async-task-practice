ec3807a3b6bf60e6bb98f186bee3c558f3b5d23a
Trishala Ghetiya <trishalaghetiya@gmail.com>
2023-12-07 23:17:21 +0530

    fetching data from database using thunk function

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useEffect } from "react";
-import { uiActions } from "./store/slices/ui-slice";
 import Notification from "./components/UI/Notification";
+import { getCartData, sendCartData } from "./store/cart-actions";
 
 let isInitial = true;
 
@@ -13,47 +13,20 @@ function App() {
   const isCartShown = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
+  const isChanged = useSelector((state) => state.cart.changed);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        })
-      );
-      const response = await fetch(
-        "https://react-http-ff156-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Sending cart data failed!");
-      }
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success...",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
-    if(isInitial){
+    dispatch(getCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isInitial) {
       isInitial = false;
       return;
     }
-    sendCartData().catch((err) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error",
-          message: "Sending cart data failed!",
-        })
-      );
-    });
+    if (isChanged) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
 
   return (
